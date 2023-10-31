309e717c0d22417f3f5b9a50e4a395a20518b8b4
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-10-31 18:46:38 +0500

    Digital Clock - Functionality Implemented

---
+++ Classes/class 26 ( JavaScript - Digital Clock)/app.js
@@ -0,0 +1,25 @@
+var interval;
+
+function digialClock() {
+  var currentDate = new Date();
+  // Get the hours and minutes from the date object.
+  var hours = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds();
+  var data = {
+    hours,
+    minutes,
+    seconds,
+  };
+  console.log(data, "data---");
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
+
+function startClock() {
+  interval = setInterval(digialClock, 1000);
+}
+
+startClock();
