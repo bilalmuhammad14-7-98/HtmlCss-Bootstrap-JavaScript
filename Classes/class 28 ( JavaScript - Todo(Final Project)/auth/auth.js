8897e35193f0d0ddd97271459cf4f9f70375413c
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-11-20 14:19:33 +0500

    Todo App - SignUp process with autogenerated id setup

--- Classes/class 28 ( JavaScript - Todo(Final Project)/auth/auth.js
+++ Classes/class 28 ( JavaScript - Todo(Final Project)/auth/auth.js
@@ -5,12 +5,46 @@ function handleSignUp() {
   var password = document.getElementById("password").value;
   var confirmPassword = document.getElementById("confirmPassword").value;
 
-  var obj = {
+  if (!username || !email || !password || !confirmPassword) {
+    alert("Please fill all fields");
+    return;
+  }
+
+  if (password.length < 6) {
+    alert("Your password must be at least 6 characters long");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  var userData = JSON.parse(localStorage.getItem("users"));
+  var count = 0;
+  var lastItem = userData ? userData[userData.length - 1] : null;
+
+  console.log(userData, "userData----");
+  // if (userData) {
+  //   var lastItem = userData.pop();
+  // }
+
+  console.log(lastItem, "lastitem");
+
+  var currentUser = {
+    id: lastItem ? lastItem.id + 1 : (count = count + 1),
     username,
     email,
     password,
-    confirmPassword,
   };
 
-  console.log(obj, "data-----");
+  console.log(currentUser, userData, "currentUser-----");
+
+  if (!userData) {
+    userData = [currentUser];
+  } else {
+    userData.push(currentUser);
+  }
+
+  localStorage.setItem("users", JSON.stringify(userData));
 }
