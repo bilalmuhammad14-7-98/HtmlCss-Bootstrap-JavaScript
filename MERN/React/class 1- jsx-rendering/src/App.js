11c7822326ae6fa0ae5881bb8f4d9cded2349cc7
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-12-18 12:08:37 +0500

    Rendering JavaScript in JSX

--- MERN/React/class 1- jsx-rendering/src/App.js
+++ MERN/React/class 1- jsx-rendering/src/App.js
@@ -2,22 +2,76 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const text = "Hello World!";
+  const num = 10;
+  const isExist = false; //render nai hoga
+  const icecream = {
+    //directly render nahi hoga, lekin key ke sath
+    name: "Chocbar",
+    flavor: "Chocolate + Vanilla",
+    company: "Walls",
+  };
+
+  const buffet = [
+    "Fish Paplet",
+    "Prawns",
+    "Chkn Karahi",
+    "Sandwich",
+    "Handi",
+    "Cake",
+  ];
+
+  const restaurants = [
+    {
+      name: "Javed Nihari",
+      location: "Dastageer",
+      review: "3.5 Star",
+    },
+    {
+      name: "Kababjees Express",
+      location: "Poray Karachi me",
+      review: "5 Star",
+    },
+    {
+      name: "Flame",
+      location: "DHA",
+      review: "4 Star",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome hello
-        </a>
-      </header>
+      <h1>Basics of Rendering</h1>
+      <h2>{text}</h2>
+      <h3>{num}</h3>
+
+      <h1>Object</h1>
+      <p>{icecream.name}</p>
+      <p>{icecream.flavor}</p>
+      <p>{icecream.company}</p>
+
+      <ul>
+        {buffet.map(function (item) {
+          return <li>{item}</li>;
+        })}
+      </ul>
+
+      <table border="2">
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Review</th>
+        </tr>
+        {restaurants.map(function (item) {
+          return (
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.location}</td>
+              <td>{item.review}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
