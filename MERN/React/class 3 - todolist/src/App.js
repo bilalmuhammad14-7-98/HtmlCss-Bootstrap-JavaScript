7f349e71ed0a6ab77ee4379ab89cc8ecd042a51f
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-12-19 17:35:00 +0500

    TodoApp UI changes

--- MERN/React/class 3 - todolist/src/App.js
+++ MERN/React/class 3 - todolist/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState(["hello", "world"]);
+  const [inputText, setInputText] = useState();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>TodoList App</h4>
+
+        <input type="text" placeholder="Please input any text"></input>
+        <button type="button">Add Todo</button>
+
+        <ul>
+          {list.map(function (item) {
+            return (
+              <div>
+                <p>-----</p>
+                {item}xx
+              </div>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
