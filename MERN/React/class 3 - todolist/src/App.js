a3d6c87afa41f80af65c3725b9706c1c997dd268
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-12-20 13:12:10 +0500

    Add and delete todo functionality implemented

--- MERN/React/class 3 - todolist/src/App.js
+++ MERN/React/class 3 - todolist/src/App.js
@@ -5,21 +5,59 @@ import { useState } from "react";
 function App() {
   const [list, setList] = useState(["hello", "world"]);
   const [inputText, setInputText] = useState();
+
+  function updateText(event) {
+    console.log(event.target.value);
+    let value = event.target.value;
+    setInputText(value);
+  }
+
+  function addTodo() {
+    console.log(inputText, "input text");
+    const copyList = [...list];
+    copyList.push(inputText);
+    setList(copyList);
+  }
+
+  function deleteTodo(index) {
+    console.log(index, "index");
+    const copyList = [...list];
+    copyList.splice(index, 1);
+    setList(copyList);
+  }
+
+  function editTodo(index) {}
   return (
     <div className="App">
       <header className="App-header">
         <h4>TodoList App</h4>
 
-        <input type="text" placeholder="Please input any text"></input>
-        <button type="button">Add Todo</button>
+        <input
+          type="text"
+          placeholder="Please input any text"
+          onChange={updateText}
+          value={inputText}
+        ></input>
+        <button type="button" onClick={addTodo}>
+          Add Todo
+        </button>
 
         <ul>
-          {list.map(function (item) {
+          {list.map(function (item, index) {
             return (
-              <div>
-                <p>-----</p>
-                {item}xx
-              </div>
+              <>
+                <span>
+                  <li key={index}>
+                    {item}{" "}
+                    <button type="button" onClick={() => deleteTodo(index)}>
+                      Delete
+                    </button>
+                    <button type="button" onClick={() => editTodo(index)}>
+                      Edit
+                    </button>
+                  </li>
+                </span>
+              </>
             );
           })}
         </ul>
