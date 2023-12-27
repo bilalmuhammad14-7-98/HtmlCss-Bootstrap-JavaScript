7f1ae8f131f26efe11102a6a3e8c3538cf10700f
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-12-27 12:43:12 +0500

    Quiz App - rendered quesitons , next and restart button functionality

--- MERN/React/class 4 - quiz-app/src/App.js
+++ MERN/React/class 4 - quiz-app/src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  useEffect(function () {
+    getQuestions();
+  }, []);
+
+  function getQuestions() {
+    fetch("https://the-trivia-api.com/v2/questions")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response, "response-----");
+        setQuestions(response);
+      });
+  }
+
+  console.log(questions, "questions-----");
+
+  function handleNext() {
+    setIndex((prev) => prev + 1);
+  }
+
+  function handleRestart() {
+    setIndex(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quiz Applicaiton</h1>
+
+        {questions.length ? (
+          <>
+            <h2>
+              Q-{index + 1}
+              {questions[index].question.text}
+            </h2>
+          </>
+        ) : (
+          <>
+            <h2>Loading...........</h2>
+          </>
+        )}
+
+        {index == questions.length - 1 ? (
+          <>
+            <button onClick={handleRestart}>Restart</button>
+          </>
+        ) : (
+          <>
+            {" "}
+            <button onClick={handleNext}>NEXT</button>
+          </>
+        )}
       </header>
     </div>
   );
