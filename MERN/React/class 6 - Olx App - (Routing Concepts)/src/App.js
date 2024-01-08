6df8741261696cada8b0e8e5a714adee9e2df6b2
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2024-01-08 12:04:44 +0500

    Olx App - Adding Bootstrap

--- MERN/React/class 6 - Olx App - (Routing Concepts)/src/App.js
+++ MERN/React/class 6 - Olx App - (Routing Concepts)/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
