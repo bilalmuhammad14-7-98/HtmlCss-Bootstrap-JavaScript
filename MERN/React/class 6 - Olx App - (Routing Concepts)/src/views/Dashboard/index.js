acc8aa4aafc1dbf67579c742472767be59df3a68
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2024-01-08 17:39:23 +0500

    Olx App - Icons library added

--- MERN/React/class 6 - Olx App - (Routing Concepts)/src/views/Dashboard/index.js
+++ MERN/React/class 6 - Olx App - (Routing Concepts)/src/views/Dashboard/index.js
@@ -1,7 +1,29 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Dashboard() {
   return (
     <>
       <h1>Dashboard Page</h1>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </span>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+
+      <div class="form-group has-search">
+        <span class="fa fa-search form-control-feedback">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </span>
+        <input type="text" class="form-control" placeholder="Search" />
+      </div>
     </>
   );
 }
