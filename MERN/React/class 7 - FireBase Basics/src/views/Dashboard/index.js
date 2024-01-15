af0e4451d7222bab821ee76a8d41772f82354b3e
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2024-01-15 11:53:42 +0500

    Firebase App - Signup and login button added on dashboard page

--- MERN/React/class 7 - FireBase Basics/src/views/Dashboard/index.js
+++ MERN/React/class 7 - FireBase Basics/src/views/Dashboard/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div>Dashboard Page</div>
+
+      <button onClick={() => navigate("/login")}> Login</button>
+      <button onClick={() => navigate("/register")}> Sign Up</button>
     </>
   );
 };
