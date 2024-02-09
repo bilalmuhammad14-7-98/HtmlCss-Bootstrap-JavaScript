2d084f9cbd06ef0267f103f04c11db0ecffb9e9d
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2024-02-09 12:19:08 +0500

    Redux Toolkit - Header Component designed

--- MERN/React/class 8 - Redux-Toolkit/src/App.js
+++ MERN/React/class 8 - Redux-Toolkit/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Router from "./config/router";
 import { Provider } from "react-redux";
 import store from "./store";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <Header />
         <Router />
       </div>
     </Provider>
