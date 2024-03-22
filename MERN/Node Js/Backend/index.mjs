2a5671824ec29ca852dcd39baeedeab06275d3f2
Muhammad Bilal <73994681+bilalmuhammad14-7-98@users.noreply.github.com>
2024-03-23 01:38:36 +0500

    Node js - Basic api call

--- MERN/Node Js/Backend/index.mjs
+++ MERN/Node Js/Backend/index.mjs
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use("/products", (req, res) => {
+  // step1 db say data lai ayega
+  res.send({
+    message: "products fetched successfully",
+    data: [{ title: "welcome" }],
+  });
+});
+
 app.listen(3001, () => {
-  console.log("Server is listening on port 3002");
+  console.log("Server is listening on port 3001");
 });
